eca2d6e7b0d61868b9237f7255e211ed0c520940
Misha Behersky <bmwant@gmail.com>
2022-04-11 16:03:39 +0200

    Update reset button :telescope:

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -170,8 +170,8 @@ export default function Converter() {
           </dd>
         </div>
         <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-          <button type="submit" className="bg-gray-500 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Calculate
+          <button type="submit" className="bg-gray-100 hover:bg-gray-200 border border-black rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Reset
           </button>
           <button type="submit" className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Save transaction
